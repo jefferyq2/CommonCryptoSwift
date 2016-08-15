7f7373f5ba406a428aef0edab4c69eb89900d27b
Chris Amanse <chris@chrisamanse.xyz>
2016-08-15 09:58:56 +0800

    Initial commit

---
+++ Sources/CommonCryptoSwift.h
@@ -0,0 +1,19 @@
+//
+//  CommonCryptoSwift.h
+//  CommonCryptoSwift
+//
+//  Created by Chris Amanse on 15/08/2016.
+//
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for CommonCryptoSwift.
+FOUNDATION_EXPORT double CommonCryptoSwiftVersionNumber;
+
+//! Project version string for CommonCryptoSwift.
+FOUNDATION_EXPORT const unsigned char CommonCryptoSwiftVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <CommonCryptoSwift/PublicHeader.h>
+
+
